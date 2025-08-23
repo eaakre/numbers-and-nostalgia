60633589075136768c10bf630d65c423c8c152f4
Erik Aakre <erikaakre@hotmail.com>
2025-08-23 17:42:31 -0500

    add table of contents to request

--- src/app/articles/[slug]/page.tsx
+++ src/app/articles/[slug]/page.tsx
@@ -78,6 +78,7 @@ export default async function ArticlePage({ params }: Props) {
         author->{name}
       },
       ctaBlocks,
+      showTableOfContents,
       showNewsletterSignup
     }
   `,
